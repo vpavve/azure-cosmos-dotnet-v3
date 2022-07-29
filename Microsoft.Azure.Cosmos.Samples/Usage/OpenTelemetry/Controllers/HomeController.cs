6dd9fc46f708ff6012aff8ca33c691e31075d678
Sourabh Jain <sourabhjain@microsoft.com>
2022-07-29 10:03:51 +0530

    refactored sample

--- Microsoft.Azure.Cosmos.Samples/Usage/OpenTelemetry/Controllers/HomeController.cs
+++ Microsoft.Azure.Cosmos.Samples/Usage/OpenTelemetry/Controllers/HomeController.cs
@@ -14,37 +14,14 @@
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> logger;
-        private readonly Container container;
-        
-        public HomeController(ILogger<HomeController> logger, Container container)
+
+        public HomeController(ILogger<HomeController> logger)
         {
             this.logger = logger;
-            this.container = container;
         }
 
         public IActionResult Index()
         {
-            Task.Run(async () =>
-            {
-                // Create an item
-                ToDoActivity testItem = ToDoActivity.CreateRandomToDoActivity("MyTestPkValue");
-                ItemResponse<ToDoActivity> createResponse = await this.container.CreateItemAsync<ToDoActivity>(testItem);
-                ToDoActivity testItemCreated = createResponse.Resource;
-
-                // Read an Item
-                await this.container.ReadItemAsync<ToDoActivity>(testItem.id, new Microsoft.Azure.Cosmos.PartitionKey(testItem.id));
-
-                // Upsert an Item
-                await this.container.UpsertItemAsync<ToDoActivity>(testItem);
-
-                // Replace an Item
-                await this.container.ReplaceItemAsync<ToDoActivity>(testItemCreated, testItemCreated.id.ToString());
-
-                // Delete an Item
-                await this.container.DeleteItemAsync<ToDoActivity>(testItem.id, new Microsoft.Azure.Cosmos.PartitionKey(testItem.id));
-
-            });
-
             return this.View();
         }
 
